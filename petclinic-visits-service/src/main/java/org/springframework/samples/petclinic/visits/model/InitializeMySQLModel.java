f1779e0ec676672fbd4c4c3e71ff778ebbf58dfa
Eitan Suez <eitan@tetrate.io>
2022-11-21 10:50:54 -0600

    looks like spring updated its data initialization conventions

--- petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/model/InitializeMySQLModel.java
+++ petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/model/InitializeMySQLModel.java
@@ -3,7 +3,6 @@ package org.springframework.samples.petclinic.visits.model;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
@@ -15,8 +14,8 @@ public class InitializeMySQLModel {
 	@GetMapping("/init")
 	public DataSourceInitializer dataSourceInitializer(@Qualifier("dataSource") final DataSource dataSource) {
 	    ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-	    resourceDatabasePopulator.addScript(new ClassPathResource("db/mysql/schema.sql"));
-	    resourceDatabasePopulator.addScript(new ClassPathResource("db/mysql/data.sql"));
+	    resourceDatabasePopulator.addScript(new ClassPathResource("schema-mysql.sql"));
+	    resourceDatabasePopulator.addScript(new ClassPathResource("data-mysql.sql"));
 	    DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
 	    dataSourceInitializer.setDataSource(dataSource);
 	    dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
