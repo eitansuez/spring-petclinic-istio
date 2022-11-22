f06592340663437fcb5c3d9ec8d7832756c950c8
Eitan Suez <eitan@tetrate.io>
2022-11-22 16:29:14 -0600

    removed workaround for a bug that has since been fixed

--- petclinic-frontend/src/main/java/org/springframework/samples/petclinic/api/PetClinicApplication.java
+++ petclinic-frontend/src/main/java/org/springframework/samples/petclinic/api/PetClinicApplication.java
@@ -1,18 +1,10 @@
 package org.springframework.samples.petclinic.api;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 public class PetClinicApplication {
@@ -29,20 +21,6 @@ public class PetClinicApplication {
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
-    }
-
-    @Value("classpath:/static/index.html")
-    private Resource indexHtml;
-
-    /**
-     * workaround solution for forwarding to index.html
-     * @see <a href="https://github.com/spring-projects/spring-boot/issues/9785">#9785</a>
-     */
-    @Bean
-    RouterFunction<?> routerFunction() {
-        return RouterFunctions.resources("/**", new ClassPathResource("static/"))
-            .andRoute(RequestPredicates.GET("/"),
-                request -> ServerResponse.ok().contentType(MediaType.TEXT_HTML).bodyValue(indexHtml));
     }
 
 }
