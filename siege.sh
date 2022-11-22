06cc1e768c23d9b5ca8e16893dcf8a614b178181
Eitan Suez <eitan@tetrate.io>
2022-11-22 10:20:48 -0600

    fixed issue with urls to target for load generation

--- siege.sh
+++ siege.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-#siege --concurrent=3 --delay=3 --file=./urls.txt
-
-siege --concurrent=3 --delay=3 http://localhost/#!/owners/details/6
+siege --concurrent=3 --delay=3 --file=./urls.txt
