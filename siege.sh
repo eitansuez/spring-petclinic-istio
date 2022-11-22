d1ae168d26e9daa2a2986deaf5171975ca185356
Eitan Suez <eitan@tetrate.io>
2022-11-21 19:41:00 -0600

    blah

--- siege.sh
+++ siege.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-siege --concurrent=3 --delay=3 --file=./urls.txt
+#siege --concurrent=3 --delay=3 --file=./urls.txt
+
+siege --concurrent=3 --delay=3 http://localhost/#!/owners/details/6
